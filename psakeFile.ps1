d9772820d937d0a3f8eefc4b448b2341de72ed1e
Jimmy Briggs <jimmy.briggs@noclocks.dev>
2025-05-30 11:18:40 -0400

    build: enhance build process with additional tasks and preferences
    Introduces new build properties for better output management and
    adds multiple tasks to streamline the workflow using
    PowerShellBuild module. This includes Clean, Build, Pester,
    Analyze, and Publish tasks, enhancing build automation and
    versatility, adhering to the minimum version requirement of 0.6.1.

--- psakeFile.ps1
+++ psakeFile.ps1
@@ -3,8 +3,16 @@ properties {
     $PSBPreference.Build.CompileModule = $false
     $PSBPreference.Help.DefaultLocale = 'en-US'
     $PSBPreference.Test.OutputFile = 'out/testResults.xml'
+    $PSBPreference.Test.OutputFormat = 'NUnitXml'
+    $PSBPreference.General.SrcRootDir = 'PSSystemDiagnostics'
+    $PSBPreference.Build.ModuleOutDir = './Output'
 }
 
 task Default -depends Test
 
+task Clean -FromModule PowerShellBuild -minimumVersion '0.6.1'
+task Build -FromModule PowerShellBuild -minimumVersion '0.6.1'
 task Test -FromModule PowerShellBuild -minimumVersion '0.6.1'
+task Pester -FromModule PowerShellBuild -minimumVersion '0.6.1'
+task Analyze -FromModule PowerShellBuild -minimumVersion '0.6.1'
+task Publish -FromModule PowerShellBuild -minimumVersion '0.6.1'
